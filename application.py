23a08a648fbb881eb33d02bf4794d63b1437cf32
koustubh muktibodh <koustub28@hotmail.com>
2026-07-06 00:10:49 -0400

    changes to application file

--- application.py
+++ application.py
@@ -40,4 +40,8 @@ def predict_datapoint():
         return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+
+    # Render dynamically assigns a port via environment variables
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
